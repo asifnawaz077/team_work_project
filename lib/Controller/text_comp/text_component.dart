77a981b7e68833baa120ab7475fba787f371e787
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-09 12:26:56 +0500

    removing size+weight

--- lib/Controller/text_comp/text_component.dart
+++ lib/Controller/text_comp/text_component.dart
@@ -3,17 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 class CustomText extends StatelessWidget {
   final String label;
-  final headingStyle = GoogleFonts.roboto(
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-    color: Color(0xff27252E)
-  );
+  final headingStyle = GoogleFonts.jost();
 
-  final subheadingStyle = GoogleFonts.jost(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-    color: Color(0xff000000),
-  );
+  final subheadingStyle = GoogleFonts.roboto();
   CustomText({super.key, required this.label});
 
   @override
