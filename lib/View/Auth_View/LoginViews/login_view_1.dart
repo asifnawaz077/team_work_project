5c5adbdfb7a4044de9699c12467f81ed1f50cae7
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-09 11:48:34 +0500

    login1

---
+++ lib/View/Auth_View/LoginViews/login_view_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginView1 extends StatefulWidget {
+  const LoginView1({super.key});
+
+  @override
+  State<LoginView1> createState() => _LoginView1State();
+}
+
+class _LoginView1State extends State<LoginView1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
