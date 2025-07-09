633dc0347feaf4c8155bc59d0a919b5b525ceb38
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-09 17:55:14 +0500

    push

--- lib/View/Auth_View/LoginViews/login_view_4.dart
+++ lib/View/Auth_View/LoginViews/login_view_4.dart
@@ -5,22 +5,21 @@ import 'package:team_work_project/Controller/textformfield/textform_field.dart';
 
 import '../../../Controller/text_comp/text_component.dart';
 
-class LoginView3 extends StatefulWidget {
-  const LoginView3({super.key});
+class LoginView4 extends StatefulWidget {
+  const LoginView4({super.key});
 
   @override
-  State<LoginView3> createState() => _LoginView3State();
+  State<LoginView4> createState() => _LoginView4State();
 }
 
-class _LoginView3State extends State<LoginView3> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+class _LoginView4State extends State<LoginView4> {
+  TextEditingController emailController=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: CustomText(
-          label: 'Log into account',
+          label: 'Reset password',
           size: 20.0,
           weight: FontWeight.w400,
           fontType: GoogleFonts.jost,
@@ -31,36 +30,21 @@ class _LoginView3State extends State<LoginView3> {
       body: Center(
         child: Column(
           children: [
-            SizedBox(
-              height: 50,
-            ),
-            CustomTextField(
-              hintText: 'alexalexander@gmail.com',
-              controller: emailController,
-              labelText: 'Email',
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            CustomTextField(
-              hintText: '********',
-              controller: passwordController,
-              labelText: 'Password',
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            BlueButtonWidget(onPressed: () {}, text: 'Log in'),
-            SizedBox(
-              height: 30,
-            ),
+            SizedBox(height: 50,),
             CustomText(
-                label: 'Forgot password?',
-                size: 14,
-                weight: FontWeight.w600,
-                fontType: GoogleFonts.jost),
+              label: 'We will email you',
+              size: 16,
+              weight: FontWeight.w400,
+              fontType: GoogleFonts.roboto,
+              color: Color(0xff27252E),
+            ),
+            CustomText(label: 'a link to reset your passsword', size: 16, weight: FontWeight.w400, fontType: GoogleFonts.roboto),
+            SizedBox(height: 40,),
+            CustomTextField(hintText: 'alexalexander@gmail.com', controller:emailController,labelText: 'Email', ),
+            SizedBox(height: 25,),
+            BlueButtonWidget(onPressed: (){}, text: 'Send'),
             SizedBox(
-              height: 190,
+              height: 238,
             ),
             CustomText(
                 label: 'By using Solvbox, you agree to the',
