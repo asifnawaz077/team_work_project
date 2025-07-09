9dfc8a735c98128837a7b8b6450ecba8325dcca1
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-09 15:00:10 +0500

    working on screen

--- lib/View/Auth_View/LoginViews/login_view_1.dart
+++ lib/View/Auth_View/LoginViews/login_view_1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:team_work_project/Controller/constentes/blue_button/blue_button_widget.dart';
 import 'package:team_work_project/Controller/text_comp/text_component.dart';
 
 class LoginView1 extends StatefulWidget {
@@ -28,6 +29,50 @@ class _LoginView1State extends State<LoginView1> {
             weight: FontWeight.bold,
             fontType: GoogleFonts.jost,
             color: Colors.black,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          CustomText(
+            label: 'Welcome to Solvbox',
+            size: 28,
+            weight: FontWeight.w400,
+            fontType: GoogleFonts.jost,
+            color: Color(0xff000000),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: BlueButtonWidget(
+              onPressed: () {},
+              text: 'Continue as problem solver',
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: BlueButtonWidget(
+              onPressed: () {},
+              text: 'Continue as problem seeker',
+            ),
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomText(
+                label: 'Already have a account?',
+                size: 16,
+                weight: FontWeight.w400,
+                fontType: GoogleFonts.jost,
+              ),CustomText(
+                  label: ' Log in',
+                  size: 16,
+                  weight: FontWeight.w600,
+                  fontType: GoogleFonts.jost)
+            ],
           )
         ],
       ),
