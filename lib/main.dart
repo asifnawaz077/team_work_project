f15928871bbfb68f7f734d67ab13ef93ad9a457d
asifnawaz077 <kgul3532@gmail.com>
2025-07-10 14:11:06 +0500

    main_dart_change

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:team_work_project/View/Auth_View/LoginViews/login_view_1.dart';
-import 'package:team_work_project/View/Auth_View/LoginViews/login_view_3.dart';
-import 'package:team_work_project/View/Auth_View/LoginViews/login_view_4.dart';
-import 'package:team_work_project/View/Auth_View/LoginViews/login_view_5.dart';
-import 'package:team_work_project/View/Auth_View/SignUpViews/signUp_view_1.dart';
-import 'package:team_work_project/View/Auth_View/SignUpViews/signup_view_2.dart';
 import 'package:team_work_project/View/Auth_View/Splash_View/Splash_View.dart';
 
-import 'package:team_work_project/View/Auth_View/LoginViews/login_view_2.dart';
-import 'package:team_work_project/View/profile_seeker_view/profile_seeker_1.dart';
-import 'package:team_work_project/View/profile_seeker_view/profile_seeker_2.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-<<<<<<< HEAD
-        home:
-            SplashView() //LoginView1()//LoginView4()//LoginView3()//LoginView2()//SplashView()
-        );
-=======
-        home: ProfileSeeker2()//LoginView1()//LoginView4()//LoginView3()//LoginView2()//SplashView()
-     );
->>>>>>> 37857c46312b3740cc866a2c40bfdb0eb34cc32a
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
   }
 }
