20c606a3e781078472442906113ff3809d56f311
asifnawaz077 <kgul3532@gmail.com>
2025-07-11 12:01:53 +0500

    .

--- lib/Controller/textformfield/textform_field.dart
+++ lib/Controller/textformfield/textform_field.dart
@@ -6,74 +6,45 @@ class CustomTextField extends StatelessWidget {
   final bool isPassword;
   final TextInputType keyboardType;
   final IconData? prefixIcon;
+  final IconData? suffixIcon;
   final double? width;
   final String? labelText;
-  final IconData? suffixIcon;
 
-
-  CustomTextField(
-      {super.key,
-<<<<<<< Updated upstream
-      required this.hintText,
-      required this.controller,
-      this.isPassword = false,
-      this.keyboardType = TextInputType.text,
-      this.prefixIcon,
-      this.width,
-      this.labelText});
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.width,
+    this.labelText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: width ?? 343,
       child: TextFormField(
-          controller: controller,
-          obscureText: isPassword,
-          keyboardType: keyboardType,
-          decoration: InputDecoration(
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: hintText,
-            labelText: labelText,
-            prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-            contentPadding: EdgeInsets.symmetric(
-              vertical: 16,
-              horizontal: 12,
-            ),
-          )),
+        controller: controller,
+        obscureText: isPassword,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          hintText: hintText,
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
+          suffixIcon: suffixIcon != null ? Icon(suffixIcon) : null,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 12,
+          ),
+        ),
+      ),
     );
-=======
-        required this.hintText,
-        required this.controller,
-        this.isPassword = false,
-        this.keyboardType = TextInputType.text,
-        this.prefixIcon,this.width,this.labelText,this.suffixIcon});
-
-  @override
-  Widget build(BuildContext context) {
-    return
-        SizedBox(
-          width: width ?? 343,
-          child: TextFormField(
-              controller: controller,
-              obscureText: isPassword,
-              keyboardType: keyboardType,
-              decoration: InputDecoration(
-                suffixIcon: suffixIcon != null ? Icon(suffixIcon) : null,
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                hintText: hintText,
-                labelText: labelText,
-                prefixIcon: prefixIcon!=null?Icon(prefixIcon):null,
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                ),
-
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 16,
-                  horizontal: 12,
-                ),
-              )),
-        );
->>>>>>> Stashed changes
   }
 }
--- lib/View/Auth_View/SignUpViews/signup_view_5.dart
+++ lib/View/Auth_View/SignUpViews/signup_view_5.dart
@@ -10,56 +10,58 @@ class SignupView5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 150,
-          ),
-          Center(
-            child: Image.asset(
-              'assets/Vector.png',
-              height: 85,
-              width: 85,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 150,
             ),
-          ),
-          SizedBox(
-            height: 70,
-          ),
-          CustomText(
-              label: 'Your account ',
-              size: 24,
-              weight: FontWeight.w400,
-              fontType: GoogleFonts.jost),
-          CustomText(
-              label: 'was successfully created! ',
-              size: 24,
-              weight: FontWeight.w400,
-              fontType: GoogleFonts.jost),
-          SizedBox(
-            height: 30,
-          ),
-          BlueButtonWidget(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SignupView6()));
-              },
-              text: 'Login'),
-          SizedBox(
-            height: 150,
-          ),
-          CustomText(
-            label: 'By using Solvbox, you agree to the',
-            size: 14,
-            weight: FontWeight.w600,
-            fontType: GoogleFonts.roboto,
-          ),
-          CustomText(
-            label: 'Terms and Privacy Policy.',
-            size: 14,
-            weight: FontWeight.w600,
-            fontType: GoogleFonts.roboto,
-          ),
-        ],
+            Center(
+              child: Image.asset(
+                'assets/Vector.png',
+                height: 85,
+                width: 85,
+              ),
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            CustomText(
+                label: 'Your account ',
+                size: 24,
+                weight: FontWeight.w400,
+                fontType: GoogleFonts.jost),
+            CustomText(
+                label: 'was successfully created! ',
+                size: 24,
+                weight: FontWeight.w400,
+                fontType: GoogleFonts.jost),
+            SizedBox(
+              height: 30,
+            ),
+            BlueButtonWidget(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignupView6()));
+                },
+                text: 'Login'),
+            SizedBox(
+              height: 150,
+            ),
+            CustomText(
+              label: 'By using Solvbox, you agree to the',
+              size: 14,
+              weight: FontWeight.w600,
+              fontType: GoogleFonts.roboto,
+            ),
+            CustomText(
+              label: 'Terms and Privacy Policy.',
+              size: 14,
+              weight: FontWeight.w600,
+              fontType: GoogleFonts.roboto,
+            ),
+          ],
+        ),
       ),
     );
   }
