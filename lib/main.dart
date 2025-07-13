152f675a31958c60468d98bec4f8cb02d8b2511a
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-13 16:03:08 +0500

    push

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:team_work_project/View/Home_View/home_view_1.dart';
 import 'package:team_work_project/View/Home_View/home_view_2.dart';
 import 'package:team_work_project/View/chat_screen/chat-screen-1.dart';
 import 'package:team_work_project/View/chat_screen/chat-screen-2.dart';
+import 'package:team_work_project/View/chat_screen/chat-screen-3.dart';
 import 'package:team_work_project/View/profile_seeker_view/profile_seeker_1.dart';
 import 'package:team_work_project/View/profile_seeker_view/profile_seeker_2.dart';
 import 'package:team_work_project/View/profile_seeker_view/profile_seeker_3.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       debugShowCheckedModeBanner: false,
-      home: ChatScreen2()//HomeView2()//SplashView(),
+      home: ChatScreen3()//HomeView2()//SplashView(),
     );
   }
 }
